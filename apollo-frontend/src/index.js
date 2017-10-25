161e9e32917a942e367db6ea9c9708f16165d2af
Pablo Chiappetti <pchiappetti@gmail.com>
2017-10-25 17:07:35 -0300

    agregar soporte a subscriptions con ws

--- apollo-frontend/src/index.js
+++ apollo-frontend/src/index.js
@@ -9,9 +9,30 @@ import ApolloClient from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloProvider } from 'react-apollo'
+import { WebSocketLink } from 'apollo-link-ws'
+import { split } from 'apollo-link'
+import { getMainDefinition } from 'apollo-utilities'
+
+const httpLink = new HttpLink({ uri: 'http://localhost:4000/graphql' })
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/subscriptions`,
+  options: {
+    reconnect: true
+  }
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLink,
+);
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://localhost:4000/graphql' }),
+  link,
   cache: new InMemoryCache().restore({})
 })
 
