f4273220daa1981cccb24c8f2b87b5e79d2ff42a
Pablo Chiappetti <pchiappetti@gmail.com>
2017-10-25 01:08:32 -0300

    agregar apollo client al bootstrap de la app

--- apollo-frontend/src/index.js
+++ apollo-frontend/src/index.js
@@ -4,5 +4,21 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// Apollo
+import ApolloClient from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloProvider } from 'react-apollo'
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:4000/graphql' }),
+  cache: new InMemoryCache().restore({})
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
+registerServiceWorker()
